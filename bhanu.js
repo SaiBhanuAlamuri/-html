0b86c8d0e833d0cdc11fdb0d2b87cb48d22e4c74
SaiBhanuAlamuri <119839820+SaiBhanuAlamuri@users.noreply.github.com>
2023-07-20 20:47:25 +0530

    Add files via upload

---
+++ bhanu.js
@@ -0,0 +1,75 @@
+
+//Switch case
+/*
+day = "tuesday"
+switch (day) {
+    case 'monday':
+        console.log("hai how are u")
+        break;
+    case 'tuesday':
+        console.log("tueday")
+        break;
+    case 'wednesday':
+        console.log("wednesday")
+        break;
+
+
+}
+*/
+
+
+/*
+
+
+//Expressions
+
+3 + 4
+1991
+true && false && !false
+if (23 > 10) {
+    const str = '23 is bigger';
+}
+//Statements are different from expression(templates accept only expression(returns certain value) not statements)
+
+
+
+//Ternary operator(conditional operator==>always produces value so it is expression)
+
+const age = 15
+age >= 18 ? console.log("I Like to drive") : console.log("I Like to drive cycle")
+
+//since operator is an expression we catch the value using a variable
+
+const drive = age >= 18 ? "I Like to drive" : "I Like to drive cycle"
+console.log(drive)
+
+//With out ternary
+
+let drink2;
+if (age >= 18) {
+    drink2 = 'wine🍷';
+}
+else {
+    drink2 = 'water 💦';
+}
+console.log(drink2);
+
+*/
+
+//Coding challege 4
+/*
+const bill = 40;
+const tip = (bill >= 50 && bill <= 300) ? 0.15 * bill : 0.2 * bill
+console.log(`tip:${tip} and  bill:${bill} and new bill is: ${bill + tip}`)*/
+
+/*
+//Javascript Releses ES5,ES6+
+Mocha ->Brendan Eich
+Moch ->LiveScript ->JavaScript
+Microscroft launches Javascript from  Netsacpe  and calling it Jscript
+ECMA offical standard for javascript
+ES5(Ecma script 5)
+ES6/ES2015(Ecma script 2015)
+
+*/
+
